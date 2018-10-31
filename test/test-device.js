8cd051a684c8f32dd03c5e0ac66885a796e054df
Alexey Taranets <alexey.taranets@altexsoft.com>
2018-10-31 16:12:39 +0200

    software version

--- test/test-device.js
+++ test/test-device.js
@@ -1,21 +1,4 @@
 const Id003 = require('../lib/id003.js');
-// const SerialPort = require('serialport');
-
-// const serial = new SerialPort('/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A50285BI-if00-port0', {
-//   baudRate: 115200,
-//   autoOpen: false,
-// });
-
-// serial.open(err => {
-//   console.log('Open', err);
-//   serial.on('data', data => {
-//     console.log('data');
-//   });
-// });
-
-// setTimeout(() => {
-//   serial.close();
-// }, 5000);
 
 const connection = Id003.factory({
   rs232: {
@@ -25,27 +8,19 @@ const connection = Id003.factory({
 });
 
 connection.run(() => {
-  connection.rs232.serial.on('data', data => {
-    console.log('messages', data);
-  });
-
-  connection.enable();
-  // connection.disable();
-  // connection.lightOff();
-  // connection.lightOn();
-  // connection.isCashboxOut();
-  // connection.refresh();
-
   connection.on('billRead', data => {
     console.log('billRead', data);
     connection.reject();
     // connection.stack();
   });
+  connection.on('dispatch', data => console.log('dispatch', data));
 
   connection.on('billValid', () => console.log('billValid'));
   connection.on('billRejected', () => console.log('billRejected'));
   connection.on('billRefused', () => console.log('billRefused'));
-  connection.on('standby', data => console.log('standby', data));
+  connection.on('standby', () => {
+    console.log(connection.getVersion());
+  });
   connection.on('stackerOpen', () => console.log('stackerOpen'));
   connection.on('error', err => console.log('error', err));
 });
